11c0eae5e3054778124e7b0a0383b687e04d1d93
Patryk Matuszak <pmatusza@redhat.com>
2024-05-04 16:05:00 +0200

    install apps from github using eget

---
+++ config/scripts/get-apps.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+export EGET_BIN=/usr/bin
+
+curl -o eget.sh https://zyedidia.github.io/eget.sh
+cat > eget.sums << EOF
+0e64b8a3c13f531da005096cc364ac77835bda54276fedef6c62f3dbdc1ee919 eget.sh
+EOF
+sha256sum -c eget.sums
+cd $EGET_BIN
+bash eget.sh
+
+eget jesseduffield/lazygit
+eget neovim/neovim
+eget BurntSushi/ripgrep
+eget --asset=amd64.tar go-task/task
+eget sachaos/viddy
+eget --asset x86_64-linux-musl --all nushell/nushell
+eget --asset linux_amd64.tar.gz --asset ^sig caddyserver/caddy
+eget zellij-org/zellij
+eget aristocratos/btop
+eget --asset linux-musl sxyazi/yazi
+
+#curl -sS https://starship.rs/install.sh | BIN_DIR=/usr/bin FORCE=1 VERBOSE=1 sh
